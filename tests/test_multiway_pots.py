05bf5092ef9623f246a48a22992d6a8c0344e48f
robert1471 <robertcdickson.2018@gmail.com>
2022-04-20 15:09:13 +0100

    added max_games parameter to PokerStarsCollection so whole files need not be read but can be read up to a limit.

--- tests/test_multiway_pots.py
+++ tests/test_multiway_pots.py
@@ -1,43 +1,185 @@
 from src.poker_main import *
+import pytest
 
 
 def test_always_passes():
     assert True
 
 
-def test_heads_up_single_street_betting_single_winner_a():
+@pytest.mark.parametrize("winner_cards, loser_cards", [
+
+    # High card over High card (highest)
+    (
+            [Card("Ah")],
+            [Card("Kh")]
+    ),
+
+    # High card over High card (lowest)
+    (
+            [Card("6h")],
+            [Card("3c")]
+    ),
+
+    # Pair over pair (high)
+    (
+            [Card("Ah"), Card("Ac")],
+            [Card("Kh"), Card("Kc")]
+    ),
+    # Pair over pair (lowest)
+    (
+            [Card("6h"), Card("6c")],
+            [Card("3h"), Card("3c")]
+    ),
+
+    # two pair over two pair (2 different pairs)
+    (
+            [Card("Ah"), Card("Ac"), Card("9h")],
+            [Card("3h"), Card("3c"), Card("7h")]),
+
+    # two pair over two pair (1 pair same)
+    (
+            [Card("Ah"), Card("Ac"), Card("9h")],
+            [Card("3h"), Card("3c"), Card("9h")]
+    ),
+
+    # two pair over two pair (one over pair + third pair)
+    (
+            [Card("Ah"), Card("Ac"), Card("8h")],
+            [Card("Kh"), Card("Kc"), Card("9h")]
+    ),
+
+    # two pair over two pair (one over pair + fourth pair)
+    (
+            [Card("Ah"), Card("Ac"), Card("2h")],
+            [Card("Kh"), Card("Kc"), Card("9h")]
+    ),
+
+    # set over set
+    (
+            [Card("Ah"), Card("Ac"), Card("As")],
+            [Card("Kh"), Card("Kc"), Card("Ks")]),
+
+    # set over set on board
+    (
+            [Card("Ah"), Card("Ac"), Card("As")],
+            [Card("9h"), Card("9s")]
+    ),
+
+    # straight over straight
+    (
+            [Card("Ah"), Card("Kc"), Card("Qs"), Card("Jh"), Card("Td")],
+            [Card("Kc"), Card("Qs"), Card("Jh"), Card("Td"), Card("9h")]
+    ),
+
+    # 2nd lowest straight over straight
+    (
+            [Card("2h"), Card("3c"), Card("4s"), Card("5h"), Card("6d")],
+            [Card("Ac"), Card("2s"), Card("3h"), Card("4d"), Card("5h")]
+    ),
+
+    # A-high straight over A-low straight
+    (
+            [Card("Ah"), Card("Kc"), Card("Qs"), Card("Jh"), Card("Td")],
+            [Card("Ac"), Card("2s"), Card("3h"), Card("4d"), Card("5h")]
+    ),
+
+    # flush over flush (high)
+    (
+            [Card("Ah"), Card("Kh"), Card("Jh"), Card("Th"), Card("9d")],
+            [Card("Kc"), Card("Qc"), Card("Tc"), Card("9c"), Card("8c")]
+    ),
+
+    # flush over flush (low)
+    (
+            [Card("8s"), Card("6s"), Card("4s"), Card("3s"), Card("2s")],
+            [Card("7c"), Card("6c"), Card("5c"), Card("3c"), Card("2c")]
+    ),
+
+    # boat over boat (pair over)
+    (
+            [Card("As"), Card("Ah"), Card("Ks"), Card("Kc"), Card("Kds")],
+            [Card("Qc"), Card("Qh"), Card("Qs"), Card("Jd"), Card("Js")]
+    ),
+
+    # boat over boat (pair under)
+    (
+            [Card("Qs"), Card("Qh"), Card("As"), Card("Ac"), Card("As")],
+            [Card("Ac"), Card("Ah"), Card("Ks"), Card("Kd"), Card("Ks")]
+    ),
+
+    # quads over quads
+    (
+            [Card("As"), Card("Ah"), Card("Ad"), Card("Ac")],
+            [Card("Ks"), Card("Kh"), Card("Kd"), Card("Kc")],
+    ),
+
+    # straight flush over straight flush
+    (
+            [Card("Ks"), Card("Qs"), Card("Js"), Card("Ts"), Card("9s")],
+            [Card("Qc"), Card("Jc"), Card("Tc"), Card("9c"), Card("8c")],
+    ),
+
+    # royal flush over straight flush
+    (
+            [Card("As"), Card("Ks"), Card("Qs"), Card("Js"), Card("Ts")],
+            [Card("Kc"), Card("Qc"), Card("Jc"), Card("Tc"), Card("9c")],
+    ),
+
+    # high pair over high card (lowest)
+    # high pair over high card (highest)
+    # low pair over high card (lowest)
+    # low pair over high card (highest)
+
+    # highest two pair over high card (lowest)
+    # highest two pair over high card (highest)
+    # lowest two pair over high card (lowest)
+    # lowest two pair over high card (highest)
+
+    # highest two pair over high pair
+    # highest two pair over high pair
+    # lowest two pair over low pair
+    # lowest two pair over low pair
+
+    # high set over high card (lowest)
+    # high set over high card (highest)
+    # low set over high card (lowest)
+    # low set over high card (highest)
+
+
+])
+def test_heads_up_single_street_betting_single_winner_a(winner_cards, loser_cards):
     players = [
 
-        Player("Abseil",
+        Player("A",
                chips=10,
                current_position="BB",
                pre_flop=["check"],
                post_flop=["raise to 5", "call"],
-               cards=[Card("Ah")]),
+               cards=winner_cards),
 
-        Player("Bobseil",
+        Player("B",
                chips=10,
                current_position="SB",
                pre_flop=["call"],
-               cards=[Card("Kc")],
+               cards=loser_cards,
                post_flop=["check", "raise to 10"],
                turn=[],
                river=[]),
 
     ]
 
-    table_cards = [Card("2h"), Card("2c"), Card("2s"), Card("2d"), Card("3h")]
+    table_cards = [Card("2h"), Card("4c"), Card("5s"), Card("8d"), Card("9h")]
     game = Poker(players, table_cards=table_cards)
 
     game.play_game()
-    print(game.ranked_players)
-    assert game.winners[0].name == "Abseil"
+
+    assert game.winners[0].name == "A"
     assert game.ranked_players[0][0].chips == 20
     assert game.winners[0].chips == 20
     assert game.players[0].chips == 20
 
 
-def test_heads_up_single_street_betting_single_winner_b():
+"""def test_heads_up_single_street_betting_single_winner_b():
     players = [
 
         Player("Abseil",
@@ -62,11 +204,11 @@ def test_heads_up_single_street_betting_single_winner_b():
     game = Poker(players, table_cards=table_cards)
 
     game.play_game()
-    print(game.ranked_players)
+
     assert game.winners[0].name == "Bobseil"
     assert game.ranked_players[0][0].chips == 20
     assert game.winners[0].chips == 20
-    assert game.players[1].chips == 20
+    assert game.players[1].chips == 20"""
 
 
 class HeadsUpTests(object):
